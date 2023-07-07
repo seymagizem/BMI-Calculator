b71fa9ed62031cb0098bdbcab65f0cbe2ec124eb
Şeyma Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-07-07 12:20:35 +0300

    Update ViewController.swift

--- BMICalculator-master/BMI Calculator/Controllers/ViewController.swift
+++ BMICalculator-master/BMI Calculator/Controllers/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBOutlet weak var heightLabel: UILabel!
